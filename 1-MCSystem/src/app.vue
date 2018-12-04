495fe003b7ad7c7225647e4b127cd6f0a1d427c8
sbwxffnhc <1192137406@qq.com>
2018-12-04 19:21:11 +0800

    信息管理系统
    开始-项目及初始化
    添加了mui

---
+++ 1-MCSystem/src/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        app
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+</script>
+<style>
+    
+</style>
---
+++ 1-MCSystem/src/main.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/*引入第三方包*/
+import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
+Vue.use(Mint);
+
+import Axios from 'axios';
+//挂载原型
+Vue.prototype.$ajax = Axios;
+Axios.defaults.baseUrl = 'http://182.254.146.100:8899/api/';
+
+import Moment from 'moment';
+
+/*引入全局组件*/
+
+
+/*引入自定义组件*/
+import App from './app.vue';
+import Home from './components/home/home.vue'
+
+
+
+
+/*VueRouter:创建对象并配置路由规则！--导航*/
+let router = new VueRouter({
+	routes:[
+		{path:'/',redirect:{name:'home'}},//重定向
+		{name:'home',path:'/home',component:Home}
+	]
+})
+
+new Vue({
+	el:'#app',
+	router,
+	render:c=>c(App)
+})
+
+
+
+
+
+
+
+
+
+

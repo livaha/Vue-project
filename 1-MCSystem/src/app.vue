1759c89023bd8fd7fd6d8f859e97cee1c7105943
sbwxffnhc <1192137406@qq.com>
2018-12-05 02:14:45 +0800

    引入自定义图标

--- 1-MCSystem/src/app.vue
+++ 1-MCSystem/src/app.vue
@@ -12,19 +12,19 @@
     	<!--尾部-->
 		<nav class="mui-bar mui-bar-tab">
 			<a class="mui-tab-item mui-active" href="#tabbar">
-				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-icon icon-shouye"></span>
 				<span class="mui-tab-label">首页</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-email"></span>
+				<span class="mui-icon icon-huiyuan"></span>
 				<span class="mui-tab-label">会员</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-contact"><span class="mui-badge">9</span></span>
+				<span class="mui-icon icon-gouwuche"><span class="mui-badge">9</span></span>
 				<span class="mui-tab-label">购物车</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-map">
-				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-icon icon-chazhao"></span>
 				<span class="mui-tab-label">查找</span>
 			</a>
 		</nav>
